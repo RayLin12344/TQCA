a28cab893d5b3d814bd7d2a8044a14560bc2e653
RayLin12344 <93502719+RayLin12344@users.noreply.github.com>
2021-12-27 12:01:11 +0800

    Create JPA02.java

---
+++ JPA02.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; 
+public class JPA02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Please enter score:");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		if (a>=60) {
+			System.out.println("You pass");
+		}
+		else {
+			
+		}
+		System.out.println("End");
+
+	}
+
+}
